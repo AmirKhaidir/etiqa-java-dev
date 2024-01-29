9011f4787ce060e82cb6f8c0e37ec7bf8a870d8d
AmirKhaidir <a.khaidir98@gmail.com>
2024-01-29 23:16:19 +0800

    First Initialization

---
+++ src/main/java/com/etiqa/custpro/CustproApplication.java
@@ -0,0 +1,13 @@
+package com.etiqa.custpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/etiqa/custpro/CustproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etiqa.custpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
